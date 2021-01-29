bd74565e949a3feb33fec3fa10c4375316d66486
timera-h <timera.barry@gmail.com>
2021-01-29 11:31:49 +0100

    test

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { store } from './_helpers';
 import { App } from './App';
 
-// setup fake backend
-import { configureFakeBackend } from './_helpers';
-configureFakeBackend();
 
 render(
-    <Provider store={store}>
+    <Provider>
         <App />
     </Provider>,
     document.getElementById('app')
